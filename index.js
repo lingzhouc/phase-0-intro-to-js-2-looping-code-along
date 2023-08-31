85cfb80d4a980489ff83ae6c54513e7e0ea4d50d
LingZhouChen <lzcciao@gmail.com>
2023-08-31 15:31:09 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+
+// returns an array of thank you messages for each name
+function writeCards(names, occasion) {
+    const message = [];
+    for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+        debugger;
+    }
+    return message;
+}
+
+// alternative
+// function writeCards(names, occasion) {
+//     const message = [];
+//     for (person of names) {
+//         message.push(`Thank you, ${person}, for the wonderful ${occasion} gift!`);
+//         debugger;
+//     }
+//     return message;
+// }
+
+// logs number while counting down, starting at provided number
+function countDown(num) {
+    while (num > -1) {
+        console.log(num);
+        num--;
+    }
+}
+
+// alternative
+// function countDown(num) {
+//     for (let i = num; i > -1; i--) {
+//         console.log(i);
+//     }
+// }
